6b9fc53fa539ea56a84d3a49fa03cedbd857998b
MiguelX413 <lbpgaming27@gmail.com>
2023-01-12 19:13:59 -0800

    Reformat bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 import logging
 import os
-from typing import Optional
 
 import discord
 
 bot = discord.Bot()
+
 
 @bot.listen()
 async def on_message(message: discord.Message):
@@ -13,31 +13,31 @@ async def on_message(message: discord.Message):
         return
     if "vriska".casefold() in message.content.casefold():
         await message.reply("<:vriska:1017263376361062490>")
-          
+
+
 @bot.event
 async def on_member_join(member):
     channel = client.get_channel(980962249550213176)
     await channel.send(
-        f'Welcome to the server, {member.mention}! Enjoy your stay here.'
+        f"Welcome to the server, {member.mention}! Enjoy your stay here."
     )
 
-@bot.slash_command(name ="verify", description="Mod command to verify new users.")
-async def verify (ctx, member: discord.Member):
+
+@bot.slash_command(name="verify", description="Mod command to verify new users.")
+async def verify(ctx, member: discord.Member):
     role = discord.utils.get(ctx.guild.roles, name="member")
     await member.add_roles(role)
-    await member.send(
-        f"Congratulations, you're now verified! Welcome to the server!"
-    )
-    await ctx.respond(f"<:thumbsupdirk:1016921360674598944>")        
+    await member.send(f"Congratulations, you're now verified! Welcome to the server!")
+    await ctx.respond(f"<:thumbsupdirk:1016921360674598944>")
+
 
 @bot.user_command(name="Verify", guild_ids=[980962249550213170])
-async def userVerify (ctx, member: discord.member):
+async def user_verify(ctx, member: discord.member):
     role = discord.utils.get(ctx.guild.roles, name="member")
     await member.add_roles(role)
-    await member.send(
-        f"Congratulations, you're now verified! Welcome to the server!"
-    )
+    await member.send(f"Congratulations, you're now verified! Welcome to the server!")
     await ctx.respond(f"<:thumbsupdirk:1016921360674598944>")
+
 
 def run_bot(token: str) -> None:
     bot.run(token)
